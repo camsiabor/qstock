59ce1eaa6b35ca7a5cbecac8d471432f26a179d3
camsiabor <kkidthelordzlight@163.com>
2018-12-16 22:39:09 +0800

    reflecting

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
-
 /*
 // TODO daemon process
 // TODO actor, proactor
 // TODO micro service
 // TODO distribute
 // TODO elasticsearch mongodb
- */
+*/
 
 import (
 	"flag"
@@ -25,68 +24,69 @@ import (
 	"time"
 )
 
-
+func MainTest() {
+	main()
+}
 
 func main() {
 
-	var version = "0.0.1";
+	var version = "0.0.1"
 
-	defer qerr.SimpleRecover(0);
-	defer signalHandle();
+	defer qerr.SimpleRecover(0)
+	defer signalHandle()
 
-	var g = global.GetInstance();
-	flag.StringVar(&g.LogPath,  "log", "log", "log file path");
-	flag.StringVar(&g.ConfigPath,  "config", "config.json", "configuration file path");
-	flag.StringVar(&g.TimeZone,  "timezone", "Asia/Shanghai", "timezone");
-	flag.StringVar(&g.Mode,  "mode", dict.MODE_MASTER, "run mode");
+	var g = global.GetInstance()
+	flag.StringVar(&g.LogPath, "log", "log", "log file path")
+	flag.StringVar(&g.ConfigPath, "config", "config.json", "configuration file path")
+	flag.StringVar(&g.TimeZone, "timezone", "Asia/Shanghai", "timezone")
+	flag.StringVar(&g.Mode, "mode", dict.MODE_MASTER, "run mode")
 
 	time.LoadLocation(g.TimeZone)
 
-	g.Continue = true;
-	g.Version = version;
+	g.Continue = true
+	g.Version = version
 	g.PanicHandler = func(pan interface{}) {
-		qlog.Log(qlog.ERROR, pan);
+		qlog.Log(qlog.ERROR, pan)
 	}
-	g.SetData("global", g);
-	g.SetData("u", wrap.U);
-	g.SetData("cachem", scache.GetCacheManager());
-	g.Run();
+	g.SetData("global", g)
+	g.SetData("u", wrap.U)
+	g.SetData("cachem", scache.GetCacheManager())
+	g.Run()
 
-	var doHelp = flag.Bool( "help", false, "help");
-	var doVersion = flag.Bool( "version", false, "version");
-	flag.Parse();
-	if (*doVersion) {
-		fmt.Println("version");
+	var doHelp = flag.Bool("help", false, "help")
+	var doVersion = flag.Bool("version", false, "version")
+	flag.Parse()
+	if *doVersion {
+		fmt.Println("version")
 	}
-	if (*doHelp) {
-		flag.PrintDefaults();
-		os.Exit(0);
+	if *doHelp {
+		flag.PrintDefaults()
+		os.Exit(0)
 	}
 
-	g.Mode = strings.ToLower(g.Mode);
-	var logi = qlog.GetLogManager().GetDef();
+	g.Mode = strings.ToLower(g.Mode)
+	var logi = qlog.GetLogManager().GetDef()
 	switch g.Mode {
 	case dict.MODE_MASTER:
-		logi.FilePrefix = "m";
+		logi.FilePrefix = "m"
 	case dict.MODE_DAEMON:
-		logi.FilePrefix = "d";
+		logi.FilePrefix = "d"
 	}
 
-	var workingDir, _ = os.Getwd();
-	qlog.Log(qlog.INFO, g.Mode, "init", workingDir);
+	var workingDir, _ = os.Getwd()
+	qlog.Log(qlog.INFO, g.Mode, "init", workingDir)
 
-	var pidfilename = g.Mode + ".pid";
-	var pidfilelock = qos.NewFileLock(pidfilename);
+	var pidfilename = g.Mode + ".pid"
+	var pidfilelock = qos.NewFileLock(pidfilename)
 	if err := pidfilelock.Lock(); err != nil {
-		qlog.Log(qlog.ERROR, g.Mode, "pid file locked", pidfilename);
-		os.Exit(1);
+		qlog.Log(qlog.ERROR, g.Mode, "pid file locked", pidfilename)
+		os.Exit(1)
 	}
-	pidfilelock.WriteString(os.Getpid());
-	defer pidfilelock.UnLock();
-
+	pidfilelock.WriteString(os.Getpid())
+	defer pidfilelock.UnLock()
 
 	// [Config] ------------------------------------------------------------------------------------------------
-	if (len(g.ConfigPath) == 0) {
+	if len(g.ConfigPath) == 0 {
 		g.ConfigPath = "config.json"
 	}
 	var config, err = qconfig.ConfigLoad(g.ConfigPath, "includes")
@@ -94,17 +94,21 @@ func main() {
 		qlog.Log(qlog.FATAL, "config", "load failure", g.ConfigPath, err)
 		return
 	}
-	g.Config = config;
+	g.Config = config
 
 	switch g.Mode {
 	case dict.MODE_MASTER:
-		master(g);
+		master(g)
 	case dict.MODE_DAEMON:
-		daemon(g);
+		daemon(g)
 	}
 
 	// [cmd] --------------------------------------------------------------------------------------------
-	handleCmd();
+	if g.CycleHandler == nil {
+		handleCmd()
+	} else {
+		g.CycleHandler("suspend", g, nil)
+	}
 
 	// [release] --------------------------------------------------------------------------------------------
 	qlog.Log(qlog.INFO, g.Mode, "fin")
@@ -115,23 +119,22 @@ func signalHandle() {
 
 }
 
-
 func handleCmd() {
-	var chCmd = make(chan string, 256);
-	var g = global.GetInstance();
+	var chCmd = make(chan string, 256)
+	var g = global.GetInstance()
 	for {
-		var cmd, ok = <- chCmd;
-		if (!ok || cmd == "exit") {
-			qlog.Log(qlog.INFO, "main", "exit");
-			break;
+		var cmd, ok = <-chCmd
+		if !ok || cmd == "exit" {
+			qlog.Log(qlog.INFO, "main", "exit")
+			break
 		}
-		qlog.Log(qlog.INFO, "main", "receive cmd", cmd);
-		if (cmd == "config reload") {
-			var config, err = qconfig.ConfigLoad(g.ConfigPath, "includes");
-			if (err != nil) {
-				qlog.Log(qlog.FATAL, "config", "load failure", err);
+		qlog.Log(qlog.INFO, "main", "receive cmd", cmd)
+		if cmd == "config reload" {
+			var config, err = qconfig.ConfigLoad(g.ConfigPath, "includes")
+			if err != nil {
+				qlog.Log(qlog.FATAL, "config", "load failure", err)
 			} else {
-				g.Config = config;
+				g.Config = config
 			}
 		}
 	}

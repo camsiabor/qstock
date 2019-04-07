670242aa30c5febcd2fd7ef4eb45943908e27954
camsiabor <kkidthelordzlight@163.com>
2019-04-08 00:53:53 +0800

    stock group profile script

--- lua/sync/th/work/stock_group.lua
+++ lua/sync/th/work/stock_group.lua
@@ -27,7 +27,7 @@ opts.browser = "firefox"
 
 opts.request = true
 opts.request_from = 1
-opts.request_to   = 1
+opts.request_to   = 6
 opts.request_types = { "concept", "industry" }
 opts.request_types = { "industry" }
 opts.request_types = { "concept" }

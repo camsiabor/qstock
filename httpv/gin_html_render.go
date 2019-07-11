e33d67de27067fc245203c70a5b215bd31ffa985
calix.lam <kkidthelordzlight@163.com>
2019-07-11 11:53:37 +0800

    websocket

--- httpv/gin_html_render.go
+++ httpv/gin_html_render.go
@@ -9,90 +9,88 @@ import (
 	"time"
 )
 
-
 type HtmlInfo struct {
-	Name string;
-	Path string;
-	Content []byte;
-	ModifyTime int64;
+	Name       string
+	Path       string
+	Content    []byte
+	ModifyTime int64
 }
 
 type CustomHTMLRenderer struct {
-	Name     string
-	Data     interface{}
+	Name string
+	Data interface{}
 }
 
-var _html_content_type = []string{"text/html; charset=utf-8"}
-var _html_cache = make(map[string]*HtmlInfo);
-var _cache_mutex = sync.RWMutex{};
-
+var htmlContentTypes = []string{"text/html charset=utf-8"}
+var htmlCache = make(map[string]*HtmlInfo)
+var cacheMutex = sync.RWMutex{}
 
 func (r CustomHTMLRenderer) WriteContentType(w http.ResponseWriter) {
 	header := w.Header()
 	if val := header["Content-Type"]; len(val) == 0 {
-		header["Content-Type"] = _html_content_type;
+		header["Content-Type"] = htmlContentTypes
 	}
 }
 
 func (r CustomHTMLRenderer) Instance(name string, data interface{}) render.Render {
 	return CustomHTMLRenderer{
-		Name : name,
-		Data : data,
-	};
+		Name: name,
+		Data: data,
+	}
 }
 
 func (r CustomHTMLRenderer) Render(w http.ResponseWriter) error {
 	r.WriteContentType(w)
 
-	var err error;
-	var content []byte;
+	var err error
+	var content []byte
 
-	_cache_mutex.RLock();
-	var info = _html_cache[r.Name];
-	_cache_mutex.RUnlock();
+	cacheMutex.RLock()
+	var info = htmlCache[r.Name]
+	cacheMutex.RUnlock()
 
-	if (info == nil) {
-		var filename = r.Name;
-		var ginv *HttpServer = r.Data.(*HttpServer);
-		var filepath = ginv.Rootabs + "/page/" + filename;
+	if info == nil {
+		var filename = r.Name
+		var ginv = r.Data.(*HttpServer)
+		var filepath = ginv.Rootabs + "/page/" + filename
 		content, err = ioutil.ReadFile(filepath)
-		if (err != nil) {
+		if err != nil {
 			w.Write([]byte(err.Error()))
-			return nil;
+			return nil
 		}
-		var stat, _ = os.Stat(filepath);
-		info = new(HtmlInfo);
-		info.Name = r.Name;
-		info.Path = filepath;
-		info.Content = content;
-		info.ModifyTime = stat.ModTime().Unix();
-		_cache_mutex.Lock();
-		_html_cache[r.Name] = info;
-		_cache_mutex.Unlock();
+		var stat, _ = os.Stat(filepath)
+		info = new(HtmlInfo)
+		info.Name = r.Name
+		info.Path = filepath
+		info.Content = content
+		info.ModifyTime = stat.ModTime().Unix()
+		cacheMutex.Lock()
+		htmlCache[r.Name] = info
+		cacheMutex.Unlock()
 	} else {
-		content = info.Content;
+		content = info.Content
 	}
-	w.Write(content);
-	return nil;
+	w.Write(content)
+	return nil
 }
 
-func GinRefreshPage(refresh_interval int) {
+func GinRefreshPage(refreshInterval int) {
 	for {
-		time.Sleep(time.Duration(refresh_interval) * time.Second)
-		_cache_mutex.RLock();
-		for _, one := range _html_cache {
-			stat, err := os.Stat(one.Path);
-			if (err == nil) {
-				var newmod = stat.ModTime().Unix();
-				if (newmod > one.ModifyTime) {
+		time.Sleep(time.Duration(refreshInterval) * time.Second)
+		cacheMutex.RLock()
+		for _, one := range htmlCache {
+			stat, err := os.Stat(one.Path)
+			if err == nil {
+				var newmod = stat.ModTime().Unix()
+				if newmod > one.ModifyTime {
 					content, err := ioutil.ReadFile(one.Path)
-					if (err == nil) {
-						one.Content = content;
-						one.ModifyTime = newmod;
+					if err == nil {
+						one.Content = content
+						one.ModifyTime = newmod
 					}
 				}
 			}
 		}
-		_cache_mutex.RUnlock();
+		cacheMutex.RUnlock()
 	}
 }
